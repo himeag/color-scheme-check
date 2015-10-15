d734292cdea22bab7571afaff9a61cca9f4b6953
Meag Tessmann <himeag@gmail.com>
2015-10-15 14:34:01 -0600

    good py answer

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 def compute():
-     ans,x,y = 0,1,2
+     ans = 0
+     x = 1
+     y = 2
      while x <= 4000000:
-          if x % 2 = 0:
+          if x % 2 == 0:
               ans += x
           x, y = y, x + y
      return str(ans)
 
-if_name == "main":
-     compute()
-print()
+if __name__ == "__main__":
+     print(compute())
+
