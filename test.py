cf17a2e40d50da937e3d4189c31a21847c710a58
Meag Tessmann <himeag@gmail.com>
2015-10-15 14:31:34 -0600

    bad py answer

---
+++ test.py
@@ -0,0 +1,11 @@
+def compute():
+     ans,x,y = 0,1,2
+     while x <= 4000000:
+          if x % 2 = 0:
+              ans += x
+          x, y = y, x + y
+     return str(ans)
+
+if_name == "main":
+     compute()
+print()
